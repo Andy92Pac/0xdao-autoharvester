5ed3a421c2b37131973fbcdb5b11a6f367884fa8
Andy92Pac <andy.mpondo@gmail.com>
2022-01-30 13:02:35 +0100

    remove unused code and update README

--- contracts/AutoHarvester.sol
+++ contracts/AutoHarvester.sol
@@ -8,31 +8,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract AutoHarvester is Ownable {
-    // Info of each pool.
-    struct PoolInfo {
-        IERC20 lpToken; // Address of LP token contract.
-        uint256 allocPoint; // How many allocation points assigned to this pool. OXDs to distribute per block.
-        uint256 lastRewardTime; // Last block time that OXDs distribution occurs.
-        uint256 accOXDPerShare; // Accumulated OXDs per share, times 1e12. See below.
-    }
-
-    // Info of each user.
-    struct UserInfo {
-        uint256 amount; // How many LP tokens the user has provided.
-        uint256 rewardDebt; // Reward debt. See explanation below.
-        //
-        // We do some fancy math here. Basically, any point in time, the amount of OXD
-        // entitled to a user but is pending to be distributed is:
-        //
-        //   pending reward = (user.amount * pool.accOXDPerShare) - user.rewardDebt
-        //
-        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accOXDPerShare` (and `lastRewardBlock`) gets updated.
-        //   2. User receives the pending reward sent to his/her address.
-        //   3. User's `amount` gets updated.
-        //   4. User's `rewardDebt` gets updated.
-    }
-
     address public masterChef;
     address public oxd;
     uint256 public oxdPid;
